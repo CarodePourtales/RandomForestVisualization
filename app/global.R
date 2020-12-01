e6546582568bf0e92dd75945691e75dae5144a00
Caroline de Pourtales <caro.de.pourtales@gmail.com>
2020-12-01 12:24:00 +0100

    delete main, execute with global

--- app/global.R
+++ app/global.R
@@ -8,3 +8,10 @@ library(ggfortify)
 library(MASS)
 library(dplyr)
 
+
+source("ui.R")
+source("server.R")
+
+# Shiny App ---------------------------------------------------------------
+
+shinyApp(ui, server)
--- app/main.R
+++
@@ -1,7 +0,0 @@
-
-source("ui.R")
-source("server.R")
-
-# Shiny App ---------------------------------------------------------------
-
-shinyApp(ui, server)
