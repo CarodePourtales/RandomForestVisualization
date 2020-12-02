ce7a16ea54dfd724b87a1c8bcd875c04a369c421
Minh-Hoang DANG <minhhoangdang@hotmail.com>
2020-12-02 19:15:46 +0100

    UI Overhaul + Code factorization

--- app/ui_model_result.R
+++ app/ui_model_result.R
@@ -1,22 +1,26 @@
-#UI model Result - Caroline de POURTALES
+#UI model Result - Caroline de POURTALES & Minh-Hoang DANG
 
 ui_model_result_summary <- sidebarLayout(
   sidebarPanel(
-    uiOutput('class'),
-    uiOutput('predictors'),
+    selectInput("class","Select class : ", choices = c("Loading...")),
+    selectizeInput("predictors", "", c("Loading..."), multiple = T, options = list(plugins = list("remove_button"))),
+    sliderInput('split','Train rate',min=0.50,max=1.0,value=0.75,step=0.01),
     sliderInput('ntree','Number of trees to grow',min=10,max=1000,value=200,step=10),
     sliderInput('mtry','Number of variables randomly sampled as candidates at each split',min=1,max=20,value=2,step=1),
     sliderInput('nodesize','Minimum size of terminal nodes',min=1,max=50,value=10,step=1),
-    actionButton(
-      inputId = "submit_loc",
-      label = "Submit"
-    )
   ),
   mainPanel(
     tabsetPanel(
       tabPanel("Model Summary",
               h2("Model summary"),
-              verbatimTextOutput("randomForest")
+              verbatimTextOutput("randomForest"),
+              fluidRow(
+                valueBoxOutput("accuracy_rate", width = 3),
+                valueBoxOutput("sensitivity", width = 3),
+                valueBoxOutput("specificity", width = 3)),
+              fluidRow(
+                valueBoxOutput("precision", width = 3),
+                valueBoxOutput("fmesure", width = 3)),
       ),
       tabPanel("Predictors' importance",
                h2("Predictors importance on the class"),
@@ -28,26 +32,10 @@ ui_model_result_summary <- sidebarLayout(
 
 ui_model_result_prediction <- sidebarLayout(
   sidebarPanel(
-    uiOutput('axis_x'),
-    uiOutput('axis_y'),
-    actionButton(
-      inputId = "submit_loc2",
-      label = "Submit"
-    )
+    selectInput("axis_x","Select variable x : ", choices = c("Loading...")),
+    selectInput("axis_y","Select variable y : ", choices = c("Loading...")),
   ),
   mainPanel(
-    fluidRow(
-      valueBoxOutput("accuracy_rate",
-        width = 3),
-      valueBoxOutput("sensitivity",
-        width = 3),
-      valueBoxOutput("specificity",
-        width = 3)),
-    fluidRow(
-      valueBoxOutput("precision",
-        width = 3),
-      valueBoxOutput("fmesure",
-        width = 3)),
     fluidRow(
       splitLayout(cellWidths = c("50%", "50%"), plotOutput("prediction"), plotOutput("missclassified_prediction"))
     )
